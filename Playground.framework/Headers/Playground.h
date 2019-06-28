216fc2c1362fcd811f23a2fd7b32f64ded4549c0
mukesh <mukesh@aleph-labs.com>
2019-06-28 10:45:01 +0800

    add the framework and zip file

---
+++ Playground.framework/Headers/Playground.h
@@ -0,0 +1,19 @@
+//
+//  Playground.h
+//  Playground
+//
+//  Created by venveh on 27/6/19.
+//  Copyright © 2019 UOB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Playground.
+FOUNDATION_EXPORT double PlaygroundVersionNumber;
+
+//! Project version string for Playground.
+FOUNDATION_EXPORT const unsigned char PlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Playground/PublicHeader.h>
+
+
